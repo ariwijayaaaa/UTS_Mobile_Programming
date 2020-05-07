a54272cce561cbe2ba6b1fb02d7345cc674be508
Ari Wijaya <ariwijayaaaa@gmail.com>
2020-05-07 18:14:49 +0800

    Pertama

--- app/src/main/java/com/example/uts_mobile_programming/lksegitiga.java
+++ app/src/main/java/com/example/uts_mobile_programming/lksegitiga.java
@@ -3,12 +3,59 @@ package com.example.uts_mobile_programming;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class lksegitiga extends AppCompatActivity {
-
+    EditText PanjangAlas, Tinggi;
+    Button BtnHitung;
+    TextView TextKeliling, TextLuas;
+    Double alas, tinggi, luas, keliling;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lksegitiga);
+
+        PanjangAlas = (EditText) findViewById(R.id.PanjangAlas);
+        Tinggi = (EditText) findViewById(R.id.Tinggi);
+        BtnHitung = (Button) findViewById(R.id.BtnHitung);
+        TextKeliling = (TextView) findViewById(R.id.TextKeliling);
+        TextLuas = (TextView) findViewById(R.id.TextLuas);
+
+        BtnHitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stralas=((EditText) PanjangAlas).getText().toString();
+                String strtinggi=((EditText) Tinggi).getText().toString();
+                switch (v.getId()) {
+                    case R.id.BtnHitung:
+                        hideKeybaord(v);
+                        break;
+                }
+
+                if (PanjangAlas.length()==0) {
+                    PanjangAlas.setError("Tidak Boleh Kosong!");
+                }  else if (Tinggi.length()==0) {
+                    Tinggi.setError("Tidak Boleh Kosong!");
+                }
+                else {
+                    alas=Double.parseDouble(stralas);
+                    tinggi=Double.parseDouble(strtinggi);
+
+                    luas= (alas*tinggi)/2;
+                    keliling= alas+alas+alas;
+
+                    TextLuas.setText(""+luas);
+                    TextKeliling.setText(""+keliling);
+                }
+            }
+            private void hideKeybaord(View v) {
+                InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(v.getApplicationWindowToken(),0);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/uts_mobile_programming/vkubus.java
+++ app/src/main/java/com/example/uts_mobile_programming/vkubus.java
@@ -3,12 +3,61 @@ package com.example.uts_mobile_programming;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class vkubus extends AppCompatActivity {
-
+    EditText PanjangBalok, LebarBalok, TinggiBalok;
+    Button BtnHitung;
+    TextView TextVolume;
+    Double panjang, lebar, volume, tinggi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vkubus);
+
+        PanjangBalok = (EditText) findViewById(R.id.PanjangBalok);
+        LebarBalok = (EditText) findViewById(R.id.LebarBalok);
+        TinggiBalok = (EditText) findViewById(R.id.TinggiBalok);
+        BtnHitung = (Button) findViewById(R.id.BtnHitung);
+        TextVolume = (TextView) findViewById(R.id.TextVolume);
+
+        BtnHitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strpanjang=((EditText) PanjangBalok).getText().toString();
+                String strlebar=((EditText) LebarBalok).getText().toString();
+                String strtinggi=((EditText) TinggiBalok).getText().toString();
+                switch (v.getId()) {
+                    case R.id.BtnHitung:
+                        hideKeybaord(v);
+                        break;
+                }
+
+                if (PanjangBalok.length()==0) {
+                    PanjangBalok.setError("Tidak Boleh Kosong!");
+                }  else if (LebarBalok.length()==0) {
+                    LebarBalok.setError("Tidak Boleh Kosong!");
+                } else if (TinggiBalok.length()==0) {
+                    TinggiBalok.setError("Tidak Boleh Kosong!");
+                }
+                else {
+                    panjang=Double.parseDouble(strpanjang);
+                    lebar=Double.parseDouble(strlebar);
+                    tinggi=Double.parseDouble(strtinggi);
+
+                    volume= panjang*lebar*tinggi;
+
+                    TextVolume.setText(""+volume);
+                }
+            }
+            private void hideKeybaord(View v) {
+                InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(v.getApplicationWindowToken(),0);
+            }
+        });
     }
 }
